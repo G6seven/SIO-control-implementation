d361eebdc525431b5f2328168e35be3aa493a9ba
JIndřich <jindrichpolak2006@gmail.com>
2023-12-10 17:38:11 +0100

    converted into cpp

---
+++ include/ino2cpp.h
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+void DisplayIOTypeConstants();
+void debugMsgPrefx();
+void debugMsg(String msg);
+void ack();
+void checkSerial();
+void updateIOConfig(String newIOConfig);
+void reportIOTypes();
+void reportIO(bool forceReport);
+bool checkIO();
+bool validateNewIOConfig(String ioConfig);
+
